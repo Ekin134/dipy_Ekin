aae3038559fb973592f5a479c8f0749b207f8349
Ekin134 <ekin.taskin33@hotmail.com>
2023-11-13 15:13:01 +0100

    fix bug

--- dipy/reconst/angular_error.py
+++ dipy/reconst/angular_error.py
@@ -5,9 +5,12 @@ from dipy.data import get_sphere
 from dipy.reconst import shm
 from dipy.core.sphere import HemiSphere
 
-# List of spheres
-sphere = dipy.data.get_sphere("repulsion724")
-sphere_724 = get_sphere("repulsion724")             #average min angle: 7.76deg
+# List of spheres symmetric724
+#sphere = dipy.data.get_sphere("repulsion724")
+sphere = dipy.data.get_sphere("symmetric724")
+#sphere_724 = get_sphere("repulsion724")             #average min angle: 7.76deg
+sphere_724 = get_sphere("symmetric724") 
+#sphere_2890 = sphere_724.subdivide(1) 
 sphere_2890 = sphere_724.subdivide(1) 
 sphere_error= HemiSphere.from_sphere(sphere_2890)
 
@@ -26,8 +29,8 @@ def get_angular_error(sh_GT, sh_prediction, sh_order):
     sf_prediction = shm.sh_to_sf(sh_prediction, sphere=sphere_error, sh_order=sh_order, basis_type='tournier07')
     
     #return the vertex indices corresponding to local maxima of both ODF
-    _, _, peaks_GT_ind = dipy.direction.peaks.peak_directions(sf_GT, sphere_error, relative_peak_threshold=.2, min_separation_angle=20)
-    _, _, peaks_prediction_ind = dipy.direction.peaks.peak_directions(sf_prediction, sphere_error, relative_peak_threshold=.2, min_separation_angle=20)
+    _, _, peaks_GT_ind = dipy.direction.peaks.peak_directions(sf_GT, sphere_error, relative_peak_threshold=.2, min_separation_angle=15)
+    _, _, peaks_prediction_ind = dipy.direction.peaks.peak_directions(sf_prediction, sphere_error, relative_peak_threshold=.2, min_separation_angle=15)
     
     # find the minimum angle between the T and the prediction
     errors = []
@@ -38,8 +41,9 @@ def get_angular_error(sh_GT, sh_prediction, sh_order):
         for j in peaks_prediction_ind:
             err.append(ang_matrix[i,j])
             err.append(180-ang_matrix[i,j])
-            
-        errors.append(np.min(err))
-        #print("errors",errors)
         
-    return np.mean(errors) 
+        if err != []:
+            errors.append(np.min(err))
+            #print("errors",errors)
+        
+    return np.mean(errors), np.size(peaks_GT_ind) , np.size(peaks_prediction_ind)
